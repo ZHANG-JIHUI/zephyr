3bc9ad1bf36683794ceb3c8bdf01deae0a3d0c83
jihui <im.zhangjihui@gmail.com>
2023-09-04 12:33:38 +0800

    expr parser test

--- tools/expr_parser/expr_parser_test.go
+++ tools/expr_parser/expr_parser_test.go
@@ -44,12 +44,21 @@ func TestExprParser(t *testing.T) {
 	proc.Register("a", func(args []any) bool {
 		arg1 := args[0].(int)
 		arg2 := args[1].(int)
-		fmt.Println(arg1 + arg2)
+		fmt.Println("method A", arg1+arg2)
 		return false
 	})
-	proc.Register("b", func(args []any) bool { return true })
-	proc.Register("c", func(args []any) bool { return true })
-	proc.Register("d", func(args []any) bool { return true })
+	proc.Register("b", func(args []any) bool {
+		fmt.Println("method B")
+		return true
+	})
+	proc.Register("c", func(args []any) bool {
+		fmt.Println("method C")
+		return true
+	})
+	proc.Register("d", func(args []any) bool {
+		fmt.Println("method D")
+		return true
+	})
 
 	expr := "(a(1,2)||b(3))&&(c(4)&&d(5))"
 	parser := NewExprParser(expr, proc)
